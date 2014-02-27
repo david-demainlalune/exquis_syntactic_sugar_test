d0cf4f9c59e8c93b4cc22e648708fc1d28e404f1
david hodgetts <david.demainlalune@gmail.com>
2014-02-27 15:37:21 +0100

    reindent files

--- app.js
+++ app.js
@@ -1,172 +1,173 @@
 var main = function(){
 
-  var editor,
-    canvas = document.getElementById("myCanvas"),
-    ctx = canvas.getContext("2d"),
-    errorsTextField = document.getElementById("errors"),
-    currentParseObject = null,
-    animObj = null,
-    input,
-    config;
+    var editor,
+        canvas = document.getElementById("myCanvas"),
+        ctx = canvas.getContext("2d"),
+        errorsTextField = document.getElementById("errors"),
+        currentParseObject = null,
+        animObj = null,
+        input,
+        config;
 
 
-  var getMousePos = function(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
+    var getMousePos = function(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+            return {
+                x: evt.clientX - rect.left,
+                y: evt.clientY - rect.top
+            };
     };
-  }
 
-  var buildInput = function(canvas){
+    var buildInput = function(canvas){
 
-    var mousePosition = {x:null, y: null},
-        mouseDown = false,
-        mouseDownEvent = false,
-        mouseUpEvent = false;
+        var mousePosition = {x:null, y: null},
+            mouseDown = false,
+            mouseDownEvent = false,
+            mouseUpEvent = false;
 
-    var result = {
-      getMousePosition: function(){ return mousePosition; },
-      getMouseButtonDown: function(){ return mouseDownEvent; },
-      getMouseButtonUp: function(){ return mouseUpEvent; },
-      getMouseButton: function(){ return mouseDown; },
-      __postRenderCleanup: function(){
-        //TODO this is hacky
-        mouseDownEvent = false;
-        mouseUpEvent = false;
-      }
-    }
+        var result = {
+            getMousePosition: function(){ return mousePosition; },
+            getMouseButtonDown: function(){ return mouseDownEvent; },
+            getMouseButtonUp: function(){ return mouseUpEvent; },
+            getMouseButton: function(){ return mouseDown; },
+            __postRenderCleanup: function(){
+                //TODO this is hacky
+                mouseDownEvent = false;
+                mouseUpEvent = false;
+            }
+        };
 
-    canvas.addEventListener('mousemove', function(e){
-      mousePosition = getMousePos(canvas, e);
-    });
+        canvas.addEventListener('mousemove', function(e){
+            mousePosition = getMousePos(canvas, e);
+        });
 
-    canvas.addEventListener('mousedown', function(e){
-      mouseDown = true;
-      mouseDownEvent = true;
-    });
+        canvas.addEventListener('mousedown', function(e){
+            mouseDown = true;
+            mouseDownEvent = true;
+        });
 
-    document.documentElement.addEventListener('mouseup', function(e){
-      if (mouseDown){
-        mouseDown = false;
-        mouseUpEvent = true; 
-      }
-    });
+        document.documentElement.addEventListener('mouseup', function(e){
+            if (mouseDown){
+                mouseDown = false;
+                mouseUpEvent = true; 
+            }
+        });
 
-    return result;
-  };
+        return result;
+    };
 
-  var buildEditor = function(id){
-    var result = ace.edit(id);
-    result.getSession().setMode("ace/mode/javascript");
-    result.setFontSize("16px");
-    return result;
-  }
+    var buildEditor = function(id){
+        var result = ace.edit(id);
+        result.getSession().setMode("ace/mode/javascript");
+        result.setFontSize("16px");
+        return result;
+    };
 
-  var buildConfig = function(editor){
+    var buildConfig = function(editor){
 
-    var makeChangeKeyboardHandlerFunc = function(keyboardHandler){
-      return function(weWantIt){
-        if (weWantIt){
-          editor.setKeyboardHandler(keyboardHandler);
-        }else{
-          editor.setKeyboardHandler();
-        } 
-      }
-    }
+        var makeChangeKeyboardHandlerFunc = function(keyboardHandler){
+            return function(weWantIt){
+                if (weWantIt){
+                    editor.setKeyboardHandler(keyboardHandler);
+                }else{
+                    editor.setKeyboardHandler();
+                } 
+            };
+        };
 
-    return {
-      vi: makeChangeKeyboardHandlerFunc("ace/keyboard/vim"),
-      emacs: makeChangeKeyboardHandlerFunc("ace/keyboard/emacs")
-    }
-  }
+        return {
+            vi: makeChangeKeyboardHandlerFunc("ace/keyboard/vim"),
+            emacs: makeChangeKeyboardHandlerFunc("ace/keyboard/emacs")
+        };
+    };
 
 
-  var updateAnimObj = function(newParseObj){
-    //the abomination
-    var functionBody = parseExquis.buildFunctionBodyString(newParseObj),
-        makeAnim;
+    var updateAnimObj = function(newParseObj){
+        //the abomination
+        var functionBody = parseExquis.buildFunctionBodyString(newParseObj),
+            makeAnim;
 
-    try{
-      makeAnim = new Function("ctx", "input", "config", functionBody);
-    }catch (e){
-      errorsTextField.innerHTML = e.message;
-      return;
-    }
+        try{
+            makeAnim = new Function("ctx", "input", "config", functionBody);
+        }catch (e){
+            errorsTextField.innerHTML = e.message;
+            return;
+        }
 
-    try{
-      testAnimObj = makeAnim(ctx, input, config);
-    }catch (e){
-      errorsTextField.innerHTML = e.message;
-      return;
-    }
+        try{
+            testAnimObj = makeAnim(ctx, input, config);
+        }catch (e){
+            errorsTextField.innerHTML = e.message;
+            return;
+        }
 
-    if (testAnimObj.draw != null){
-      try {
-        testAnimObj.draw();
-      } catch(e){
-        errorsTextField.innerHTML = e.message;
-        return;
-      }
-    }
+        if (testAnimObj.draw != null){
+            try {
+                testAnimObj.draw();
+            } catch(e){
+                errorsTextField.innerHTML = e.message;
+                return;
+            }
+        }
 
-    animObj = testAnimObj;
+        animObj = testAnimObj;
 
-    currentParseObject = newParseObj;
-  }
+        currentParseObject = newParseObj;
+    };
 
-  var resetMatrices = function(){
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-  }
+    var resetMatrices = function(){
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+    };
 
-  var handleNewParseObject = function(newParseObj){
+    var handleNewParseObject = function(newParseObj){
 
-    if (parseExquis.isFullCodeIdentical(currentParseObject, newParseObj)){
-      return;
-    }
+        if (parseExquis.isFullCodeIdentical(currentParseObject, newParseObj)){
+            return;
+        }
 
-    updateAnimObj(newParseObj);
-  }
+        updateAnimObj(newParseObj);
+    };
 
-  var generalizeRequestAnimation = function(){
-    window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-    window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-  }
+    var generalizeRequestAnimation = function(){
+        window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+        window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    };
 
-  var onEditorChange = function(e){
-    var parseObj = parseExquis.stringToParseObject(editor.getValue());
-    if (parseObj.error !=null){
-      errorsTextField.innerHTML = parseObj.error.message;
-      return;
-    }
+    var onEditorChange = function(e){
+        var parseObj = parseExquis.stringToParseObject(editor.getValue());
 
-    errorsTextField.innerHTML = "";
-    handleNewParseObject(parseObj);
-  }
+        if (parseObj.error != null){
+            errorsTextField.innerHTML = parseObj.error.message;
+            return;
+        }
 
-  var render = function(timestamp){
-    if (animObj != null && animObj.draw != null){
-      resetMatrices();
-      try{
-        animObj.draw();
-      }catch(e){
-        errorsTextField.innerHTML = e.message;
-      } 
-    }
+        errorsTextField.innerHTML = "";
+        handleNewParseObject(parseObj);
+    };
 
-    input.__postRenderCleanup();
+    var render = function(timestamp){
+        if (animObj != null && animObj.draw != null){
+            resetMatrices();
+            try{
+                animObj.draw();
+            }catch(e){
+                errorsTextField.innerHTML = e.message;
+            } 
+        }
+
+        input.__postRenderCleanup();
+        requestAnimationFrame(render);
+    };
+
+
+    input = buildInput(canvas);
+    editor = buildEditor("editor");
+    editor.getSession().on('change', onEditorChange);
+
+    config = buildConfig(editor);
+
+    generalizeRequestAnimation();
     requestAnimationFrame(render);
-  }
-
-
-  input = buildInput(canvas);
-  editor = buildEditor("editor");
-  editor.getSession().on('change', onEditorChange);
-
-  config = buildConfig(editor);
-
-  generalizeRequestAnimation();
-  requestAnimationFrame(render);
 }
 
 
--- exquis/parseExquis.js
+++ exquis/parseExquis.js
@@ -1,170 +1,169 @@
 var parseExquis = (function(){
 
-  ///////////////////////////
-  //Private
+    ///////////////////////////
+    //Private
 
-  // Executes visitor on the object and its children (recursively).
-  // cf esprime examples
-  function traverse(object, visitor) {
-    var key, child;
+    // Executes visitor on the object and its children (recursively).
+    // cf esprime examples
+    function traverse(object, visitor) {
+        var key, child;
 
-    visitor.call(null, object);
-    for (key in object) {
-      if (object.hasOwnProperty(key)) {
-        child = object[key];
-        if (typeof child === 'object' && child !== null) {
-          traverse(child, visitor);
+        visitor.call(null, object);
+        for (key in object) {
+            if (object.hasOwnProperty(key)) {
+                child = object[key];
+                if (typeof child === 'object' && child !== null) {
+                    traverse(child, visitor);
+                }
+            }
         }
-      }
-    }
-  }
-
-  function nodeHasName(node, funcName){
-    return node.id.name == funcName;
-  }
-
-  function extractRangeFromFuncDeclaration(node){
-    return [node.range[0], node.body.range[1]];
-  }
-
-  function findFuncDeclaration(ast, funcName){
-    var result = [];
-
-    traverse(ast, function (node) {
-      if (node.type === 'FunctionDeclaration' && nodeHasName(node, funcName)) {
-        result.push(node);
-      }
-    });
-
-    return result;
-  }
-
-  function buildError(type, message){
-    return {
-      type: type,
-      message: message
-    }
-  }
-
-  function parseAst(parseObject){
-    var ast;
-    try{
-      ast = esprima.parse(parseObject.fullCodeString, { tolerant: false, loc: true, range: true}, 4);
-    }catch (e){
-      parseObject.error = buildError(errorTypes.AST, e.message);
-      return parseObject;
     }
 
-    parseObject.ast = ast;
-    return parseObject;
-  }
-
-  function parseFuncDeclaration(parseObject, funcName){
-    var nodeCandidates = findFuncDeclaration(parseObject.ast, funcName);
-
-    if (nodeCandidates.length > 1){
-      // error we only handle one func name funcName 
-      parseObject.error = buildError(errorTypes.EXQUIS_SYNTAX, "we don't handle more than one " + funcName + " function definition");
-      return parseObject;
+    function nodeHasName(node, funcName){
+        return node.id.name == funcName;
     }
 
-    if (nodeCandidates.length == 0){
-      // do nothing no candidate found
-      return parseObject;
+    function extractRangeFromFuncDeclaration(node){
+        return [node.range[0], node.body.range[1]];
     }
 
-    // happy case one func found
-    var node = nodeCandidates[0];
-    var range = extractRangeFromFuncDeclaration(node);
-    parseObject[funcName].node = node;
-    parseObject[funcName].str = parseObject.fullCodeString.substring(range[0], range[1])
+    function findFuncDeclaration(ast, funcName){
+        var result = [];
 
-    return parseObject;
-  }
+        traverse(ast, function (node) {
+            if (node.type === 'FunctionDeclaration' && nodeHasName(node, funcName)) {
+                result.push(node);
+            }
+        });
 
-  function parseDraw(parseObject){
-    return parseFuncDeclaration(parseObject, "draw");
-  }
+        return result;
+    }
+
+    function buildError(type, message){
+        return {
+            type: type,
+            message: message
+        };
+    }
+
+    function parseAst(parseObject){
+        var ast;
+
+        try{
+            ast = esprima.parse(parseObject.fullCodeString, { tolerant: false, loc: true, range: true}, 4);
+        }catch (e){
+            parseObject.error = buildError(errorTypes.AST, e.message);
+            return parseObject;
+        }
+
+        parseObject.ast = ast;
+        return parseObject;
+    }
+
+    function parseFuncDeclaration(parseObject, funcName){
+        var nodeCandidates = findFuncDeclaration(parseObject.ast, funcName);
+
+        if (nodeCandidates.length > 1){
+            // error we only handle one func name funcName 
+            parseObject.error = buildError(errorTypes.EXQUIS_SYNTAX, "we don't handle more than one " + funcName + " function definition");
+            return parseObject;
+        }
+
+        if (nodeCandidates.length == 0){
+            // do nothing no candidate found
+            return parseObject;
+        }
+
+        // happy case one func found
+        var node = nodeCandidates[0];
+        var range = extractRangeFromFuncDeclaration(node);
+        parseObject[funcName].node = node;
+        parseObject[funcName].str = parseObject.fullCodeString.substring(range[0], range[1])
+
+        return parseObject;
+    }
+
+    function parseDraw(parseObject){
+        return parseFuncDeclaration(parseObject, "draw");
+    }
 
 
-  function buildClosureBody(parseObject){
-    var result = parseObject.fullCodeString;
-    
-    if (parseObject.draw.str != null)
-      result = result.replace(parseObject.draw.str, "");
+    function buildClosureBody(parseObject){
+        var result = parseObject.fullCodeString;
+        
+        if (parseObject.draw.str != null)
+          result = result.replace(parseObject.draw.str, "");
 
-    var parseOpt = {tolerant: false, loc: false, range: false}; // in the hopes of easier ast comparison
-    parseObject.closureBodyAst = esprima.parse(result, parseOpt, 4);
-    parseObject.closureBodyStr = escodegen.generate(parseObject.closureBodyAst);
-    return parseObject;
-  }
+        var parseOpt = {tolerant: false, loc: false, range: false}; // in the hopes of easier ast comparison
+        parseObject.closureBodyAst = esprima.parse(result, parseOpt, 4);
+        parseObject.closureBodyStr = escodegen.generate(parseObject.closureBodyAst);
+        return parseObject;
+    }
 
 
-  ///////////////////
-  // public
+    ///////////////////
+    // public
 
-  function stringToParseObject(fullCodeString){
-    var result = { error: null,
-      fullCodeString: fullCodeString,
-      ast: null,
-      draw: {
-        node: null,
-        str: null
-      },
-      closureBodyStr: null,
-      closureBodyAst:null
+    function stringToParseObject(fullCodeString){
+        var result = { error: null,
+            fullCodeString: fullCodeString,
+            ast: null,
+            draw: {
+                node: null,
+                str: null
+            },
+            closureBodyStr: null,
+            closureBodyAst:null
+        };
+
+        var steps = [parseAst, parseDraw, buildClosureBody];
+
+        for(var i = 0; i < steps.length; i++) {
+            result = steps[i](result);
+            if (result.error != null){
+                return result;
+            }
+        }
+
+        return result;
+    }
+
+    function buildFunctionBodyString(parseObject){
+        var result = parseObject.closureBodyStr,
+            returnObjectProperties = [];
+
+        result += "\nreturn {\n";
+
+        if (parseObject.draw.node != null){
+            returnObjectProperties.push("\tdraw: " + parseObject.draw.str);
+        }
+
+        result += returnObjectProperties.join();
+
+        result += "\t}";
+        return result;
+    }
+
+    function isFullCodeIdentical(parseA, parseB){
+
+        if (parseA == null || parseB == null){
+            return false;
+        }
+
+        return escodegen.generate(parseA.ast) == escodegen.generate(parseB.ast);
+    }
+
+
+    var errorTypes = {
+        AST: "ast",
+        EXQUIS_SYNTAX: "exquis_syntax"
     };
 
-    var steps = [parseAst, parseDraw, buildClosureBody];
 
-    for (var i = 0; i < steps.length; i++) {
-      result = steps[i](result);
-      if (result.error != null){
-        return result;
-      }
-    }
-
-
-    return result;
-  }
-
-  function buildFunctionBodyString(parseObject){
-    var result = parseObject.closureBodyStr;
-
-    result += "\nreturn {\n";
-
-    var returnObjectProperties = [];
-
-    if (parseObject.draw.node != null){
-      returnObjectProperties.push("\tdraw: " + parseObject.draw.str);
-    }
-
-    result += returnObjectProperties.join();
-
-    result += "\t}";
-    return result;
-  }
-
-  function isFullCodeIdentical(parseA, parseB){
-
-    if (parseA == null || parseB == null){
-      return false;
-    }
-      
-    return escodegen.generate(parseA.ast) == escodegen.generate(parseB.ast);
-  }
-
-
-  var errorTypes = {
-    AST: "ast",
-    EXQUIS_SYNTAX: "exquis_syntax"
-  }
-
-
-  return {
-    stringToParseObject: stringToParseObject,
-    buildFunctionBodyString: buildFunctionBodyString,
-    isFullCodeIdentical: isFullCodeIdentical,
-    errorTypes: errorTypes
-  }
+    return {
+        stringToParseObject: stringToParseObject,
+        buildFunctionBodyString: buildFunctionBodyString,
+        isFullCodeIdentical: isFullCodeIdentical,
+        errorTypes: errorTypes
+    };
 })();
